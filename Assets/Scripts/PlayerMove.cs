c274c34d5d25465cd3e5f7c5868a9286820689b1
Carlos Cabral <carlosfc.631@gmail.com>
2020-10-23 09:36:51 -0300

    Todos los movimientos del personaje completados

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -12,6 +12,9 @@ public class PlayerMove : MonoBehaviour
     public float fallMultiplier = 0.5f;
     public float lowJumpMultiplier = 1f;
 
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
@@ -23,20 +26,40 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Run", true);
+
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Run", true);
+
         }
         else
         {
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            animator.SetBool("Run", false);
         }
 
         if (Input.GetKey("space") && Checkground.isGrounded)
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
         }
+
+        if (Checkground.isGrounded == false)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+        }
+        
+        if (Checkground.isGrounded == true)
+        {
+            animator.SetBool("Jump", false);
+            
+        }
+
         //Con esta funcion generamos un salto mas alto si se mantiene presionada la barra de espacio al saltar
         if (betterJump)
         {
