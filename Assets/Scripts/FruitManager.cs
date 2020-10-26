6be9c7d60effa358ea8f81593a6b53cdf28ad255
Carlos Cabral <carlosfc.631@gmail.com>
2020-10-26 10:06:39 -0300

    Todo arreglado y con pasajes de niveles

--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FruitManager : MonoBehaviour
 {
 
+    private void Update()
+    {
+        AllFruitsCollected();
+    }
+
     public void AllFruitsCollected()
     {
-        if (transform.childCount == 1)
+        if (transform.childCount == 0)
         {
-
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
